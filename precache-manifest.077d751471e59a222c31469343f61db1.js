d85b8f5eca86c39ffcf2f9c776d77865c7c87c9a
Dejan Miocinovic <dekimioc@yahoo.com>
2020-01-29 21:33:07 +0100

    Updates

---
+++ precache-manifest.077d751471e59a222c31469343f61db1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7e272f7dae22ea86138bce52efcabe6c",
+    "url": "/bakson/index.html"
+  },
+  {
+    "revision": "ba8d3427c7a8d6e995bc",
+    "url": "/bakson/static/css/main.aee7d8db.chunk.css"
+  },
+  {
+    "revision": "190b34c2e770fc9a0f75",
+    "url": "/bakson/static/js/2.dd9646f6.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/bakson/static/js/2.dd9646f6.chunk.js.LICENSE"
+  },
+  {
+    "revision": "ba8d3427c7a8d6e995bc",
+    "url": "/bakson/static/js/main.76e50c9a.chunk.js"
+  },
+  {
+    "revision": "d68888edb5083292cb3e",
+    "url": "/bakson/static/js/runtime-main.ca9f7e3e.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/dekimioc/bakson.git/precache-manifest.b4071f70bab43044f1959b867ea97aca.js"
+  "/bakson/precache-manifest.077d751471e59a222c31469343f61db1.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/dekimioc/bakson.git/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/bakson/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
